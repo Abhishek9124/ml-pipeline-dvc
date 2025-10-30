736fc3d7455b5770950455b89c8c7fe11d9ce473
Abhishek9124 <gangurde.8278@gmail.com>
2025-10-30 11:48:07 +0530

    params.yaml updated

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.model_selection import train_test_split
+import yaml
+
+test_size = yaml.safe_load(open('params.yaml','r'))['data_ingestion']['test_size']
 
 df=pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
 
@@ -11,7 +14,7 @@ final_df=df[df['sentiment'].isin(['happiness','sadness'])]
 
 final_df['sentiment'].replace({'happiness':1,'sadness':0},inplace=True)
 
-train_df,test_df=train_test_split(final_df,test_size=0.2,random_state=42)
+train_df,test_df=train_test_split(final_df,test_size=test_size,random_state=42)
 
 data_path=os.path.join("data","raw")
 
--- src/model_building.py
+++ src/model_building.py
@@ -3,6 +3,10 @@ import pandas as pd
 import pickle
 
 from sklearn.ensemble import GradientBoostingClassifier
+import yaml
+
+n_estimators = yaml.safe_load(open('params.yaml','r'))['model_building']['n_estimators']
+learning_rate = yaml.safe_load(open('params.yaml','r'))['model_building']['learning_rate']
 
 # fetch the data from data/processed
 train_data = pd.read_csv('./data/features/train_bow.csv')
@@ -12,7 +16,7 @@ y_train = train_data.iloc[:,-1].values
 
 # Define and train the XGBoost model
 
-clf = GradientBoostingClassifier(n_estimators=50)
+clf = GradientBoostingClassifier(n_estimators=n_estimators,learning_rate=learning_rate)
 clf.fit(X_train, y_train)
 
 # save
